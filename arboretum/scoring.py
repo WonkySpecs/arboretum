ed7fb1edb93c2daa1989975a156fa46f491428a0
Will Taylor <w1ll100@hotmail.co.uk>
2019-11-25 13:11:47 +0000

    Ones cancel eights when scoring

--- arboretum/scoring.py
+++ arboretum/scoring.py
@@ -1,22 +1,12 @@
 from collections import defaultdict
-from typing import List, Dict
+from typing import List, Dict, Set
 
 from .data import Player, Suit, Card
 
 
 def score_game(players: List[Player]) -> Dict[Player, int]:
-    suits = set()
-    for player in players:
-        for card in player.arboretum.grid.values():
-            suits.add(card.suit)
-
-    player_suit_values: Dict[Player, Dict[Suit, int]] = dict()
-    for player in players:
-        suit_values: Dict[Suit, int] = defaultdict(int)
-        for card in player.hand:
-            suit_values[card.suit] += card.value
-        player_suit_values[player] = suit_values
-
+    player_suit_values = calc_player_suit_values(players)
+    suits = set(suit for suit_scores in player_suit_values.values() for suit in suit_scores.keys())
     scores: Dict[Player, int] = defaultdict(int)
     for suit in suits:
         scorers = who_scores(suit, player_suit_values)
@@ -25,6 +15,30 @@ def score_game(players: List[Player]) -> Dict[Player, int]:
             scores[scorer] += max([score_path(p) for p in paths])
 
     return scores
+
+
+def calc_player_suit_values(players: List[Player]) -> Dict[Player, Dict[Suit, int]]:
+    # All the suits we care about scoring
+    suits: Set[Suit] = set()
+    # All the ones held in hands
+    ones: Dict[Suit, Player] = {}
+    for player in players:
+        for card in player.arboretum.grid.values():
+            suits.add(card.suit)
+        for card in player.hand:
+            if card.value == 1:
+                ones[card.suit] = player
+
+    player_suit_values: Dict[Player, Dict[Suit, int]] = dict()
+    for player in players:
+        suit_values: Dict[Suit, int] = defaultdict(int)
+        for card in player.hand:
+            # Value of 8 is ignored if another player has the 1 of the same suit
+            if card.value == 8 and card.suit in ones and ones[card.suit] != player:
+                continue
+            suit_values[card.suit] += card.value
+        player_suit_values[player] = suit_values
+    return player_suit_values
 
 
 def who_scores(suit: Suit, remaining_card_scores: Dict[Player, Dict[Suit, int]]) -> List[Player]:
@@ -49,7 +63,7 @@ def score_path(path: List[Card]) -> int:
     """
     score = len(path)
     if path[0].value == 1:
-        score +=1
+        score += 1
     if path[-1].value == 8:
         score += 2
     if len(path) >= 4 and all([c.suit == path[0].suit for c in path]):
--- arboretum/test/scoring_test.py
+++ arboretum/test/scoring_test.py
@@ -1,7 +1,7 @@
 import unittest
 
-from arboretum.data import Suit, Card
-from arboretum.game import score_path
+from arboretum.data import Suit, Card, Player
+from arboretum.scoring import score_path, calc_player_suit_values
 
 
 class GameTest(unittest.TestCase):
@@ -32,3 +32,36 @@ class GameTest(unittest.TestCase):
     def test_score_path_from_one_to_eight(self):
         path = [Card(Suit.CASSIA, 1), Card(Suit.JACARANDA, 2), Card(Suit.OAK, 4), Card(Suit.CASSIA, 8)]
         self.assertEqual(score_path(path), 7)
+
+    @staticmethod
+    def hands():
+        p1 = Player(1)
+        p1.hand = [
+            Card(Suit.WILLOW, 1),
+            Card(Suit.WILLOW, 2),
+            Card(Suit.WILLOW, 3),
+            Card(Suit.WILLOW, 4),
+            Card(Suit.WILLOW, 5),
+            Card(Suit.WILLOW, 6),
+            Card(Suit.WILLOW, 7),
+        ]
+
+        p2 = Player(2)
+        p2.hand = [
+            Card(Suit.WILLOW, 8),
+            Card(Suit.OAK, 1),
+            Card(Suit.OAK, 8),
+            Card(Suit.JACARANDA, 1),
+            Card(Suit.JACARANDA, 2),
+            Card(Suit.JACARANDA, 3),
+        ]
+        return [p1, p2]
+
+    def test_calc_player_suit_values(self):
+        [p1, p2] = self.hands()
+        values = calc_player_suit_values([p1, p2])
+        self.assertEqual(values[p1][Suit.WILLOW], 28)
+
+        self.assertEqual(values[p2][Suit.WILLOW], 0)
+        self.assertEqual(values[p2][Suit.OAK], 9)
+        self.assertEqual(values[p2][Suit.JACARANDA], 6)
