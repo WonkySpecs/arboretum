ffbc3d02fefca46036b087ffa65b8ccec476ef7b
Will Taylor <w1ll100@hotmail.co.uk>
2019-11-25 16:45:51 +0000

    Bruite force game loop with inputs, pycharm complaining about imports that work

---
+++ arboretum/__main__.py
@@ -0,0 +1,57 @@
+from typing import Tuple, Optional
+
+from data import Game, DrawTarget, Card, Suit, Pos
+from scoring import score_game
+
+
+def get_draw_target_from_input() -> Tuple[DrawTarget, Optional[int]]:
+    s_in = input("Where to draw from? 'deck' or '<player number>'")
+    if not s_in or s_in == "deck":
+        return DrawTarget.DECK, None
+    else:
+        return DrawTarget.PLAYER_DISCARD, int(s_in)
+
+
+def top_of_discards(game):
+    return str({
+        p.num: str(p.discard[-1]) if p.discard else "" for p in game.players
+    })
+
+
+def get_play_from_input():
+    print("Choose play, separated by commas: val,suit,x,y,discard_val,discard_suit")
+    (val, suit, x, y, disc_val, disc_suit) = input().split(",")
+    return Card(Suit.from_str(suit), int(val)), Pos(int(x), int(y)), Card(Suit.from_str(disc_suit), int(disc_val))
+
+
+num_players = int(input("Num players:"))
+game = Game(num_players)
+
+while not game.finished:
+    print(f"\nPlayer {game.player_turn + 1}'s turn")
+    print(f"Your arboretum:\n{game.current_player.arboretum.formatted()}")
+    for i in range(2):
+        print(f"Your hand: {game.current_player.hand}")
+        print(f"Draw {i + 1}/2, choose 'deck' or from {top_of_discards(game)}")
+        valid = False
+        while not valid:
+            draw_type, target = get_draw_target_from_input()
+            valid, message = game.is_valid_draw_target(draw_type, target)
+            if not valid:
+                print(message)
+            else:
+                game.current_player.hand.append(game.draw(draw_type, target))
+        print()
+
+    print(f"Your hand: {game.current_player.hand}")
+    valid = False
+    while not valid:
+        play_card, pos, discard_card = get_play_from_input()
+        valid, message = game.current_player.is_valid_play(play_card, pos, discard_card)
+        if not valid:
+            print(message)
+        else:
+            game.current_player.play(play_card, pos, discard_card)
+            game.player_turn = (game.player_turn + 1) % num_players
+
+print(f"Scores:\n{score_game(game.players)}")
--- arboretum/data.py
+++ arboretum/data.py
@@ -18,6 +18,37 @@ class Suit(Enum):
     LILAC = "Lilac"
     WILLOW = "Willow"
 
+    @staticmethod
+    def shortcuts() -> Dict[str, 'Suit']:
+        return {
+            "c": Suit.CASSIA,
+            "mg": Suit.MAGNOLIA,
+            "d": Suit.DOGWOOD,
+            "m": Suit.MAPLE,
+            "j": Suit.JACARANDA,
+            "r": Suit.ROYAL_POINCIANA,
+            "ol": Suit.OLIVE,
+            "o": Suit.OAK,
+            "l": Suit.LILAC,
+            "w": Suit.WILLOW,
+        }
+
+    @staticmethod
+    def from_str(s):
+        if s in Suit.shortcuts():
+            return Suit.shortcuts()[s]
+
+        for suit in Suit:
+            if suit.value == s:
+                return suit
+        raise ValueError(f"No suit '{s}'")
+
+    def short_name(self):
+        reversed_shortcuts = {
+            v: k for k, v in Suit.shortcuts().items()
+        }
+        return reversed_shortcuts[self]
+
 
 class Card:
     __slots__ = ("suit", "value")
@@ -33,6 +64,9 @@ class Card:
         if not isinstance(other, Card):
             return False
         return self.suit == other.suit and self.value == other.value
+
+    def short_format(self):
+        return f"{self.value}{self.suit.short_name():2}"
 
 
 def new_deck(num_suits: int, chosen_suits: Optional[List[Suit]] = None) -> List[Card]:
@@ -52,6 +86,12 @@ class Arboretum:
         self.grid = {}
 
     def is_valid_play_pos(self, pos: Pos) -> Tuple[bool, Optional[str]]:
+        if not self.grid:
+            if pos == Pos(0, 0):
+                return True, None
+            else:
+                return False, "First card must be at 0, 0"
+
         if pos in self.grid:
             return False, "Cannot place on top of existing card"
 
@@ -112,6 +152,25 @@ class Arboretum:
                 else:
                     queue.append((next_pos, next_path))
 
+    def formatted(self):
+        if not self.grid:
+            return ""
+        min_x = min([p.x for p in self.grid.keys()])
+        min_y = min([p.y for p in self.grid.keys()])
+        max_x = max([p.x for p in self.grid.keys()])
+        max_y = max([p.y for p in self.grid.keys()])
+
+        grid = [[f"{'':3}" for _ in range(min_x, max_x + 1)] for _ in range(min_y, max_y + 1)]
+        for pos, card in self.grid.items():
+            grid[pos.y][pos.x] = card.short_format()
+        ret = [[f"{'Row ' + str(i):6}:", *row] for i, row in enumerate(grid)]
+        col_nums = [f"{'':6}"] + [f"{i:3}" for i in range(min_x, max_x + 1)]
+        ret.insert(0, col_nums)
+        s = ""
+        for row in ret:
+            s += str(row) + "\n"
+        return s
+
 
 class Player:
     __slots__ = ("num", "hand", "discard", "arboretum")
@@ -128,11 +187,11 @@ class Player:
         self.arboretum = Arboretum()
 
     def is_valid_play(self, to_play: Card, pos: Pos, to_discard: Card) -> Tuple[bool, Optional[str]]:
-        if to_play not in self.hand:
-            return False, "Cannot play card you don't have"
+        if not any([to_play == c for c in self.hand]):
+            return False, f"Cannot play card '{to_play}' because you don't have it"
 
-        if to_discard not in self.hand:
-            return False, "Cannot discard card you don't have"
+        if not any([to_discard == c for c in self.hand]):
+            return False, f"Cannot discard card '{to_discard}' because you don't have it"
 
         return self.arboretum.is_valid_play_pos(pos)
 
@@ -146,7 +205,7 @@ class Player:
 
 class DrawTarget(Enum):
     DECK = auto()
-    DISCARD_PILE = auto()
+    PLAYER_DISCARD = auto()
 
 
 class Game:
@@ -157,7 +216,7 @@ class Game:
         self.players = [Player(i) for i in range(num_players)]
         self.deck = new_deck(self.num_suits(num_players), chosen_suits)
         self.player_turn = 0
-        self.game_over = False
+        self.finished = False
 
         for player in self.players:
             for _ in range(Game.STARTING_CARDS):
@@ -169,16 +228,24 @@ class Game:
             return num_players * 2 + 2
         raise ValueError("Number of players must be 2, 3, or 4")
 
-    def is_valid_draw_target(self, target: DrawTarget, player_num: Optional[int]) -> bool:
+    def is_valid_draw_target(self, target: DrawTarget, player_num: Optional[int] = None) -> Tuple[bool, Optional[str]]:
         if target == DrawTarget.DECK:
-            return True
-        return len(self.players[player_num].discard) > 0
+            return True, None
+        valid = len(self.players[player_num].discard) > 0
+        if valid:
+            return True, None
+        else:
+            return False, f"Cannot take from player {player_num}, they have no discard pile"
 
-    def draw(self, target: DrawTarget, player_num: Optional[int]) -> Card:
+    def draw(self, target: DrawTarget, player_num: Optional[int] = None) -> Card:
         if target == DrawTarget.DECK:
             drawn_card = self.deck.pop()
             if not self.deck:
-                self.game_over = False
+                self.finished = False
             return drawn_card
 
         return self.players[player_num].discard.pop()
+
+    @property
+    def current_player(self) -> Player:
+        return self.players[self.player_turn]
