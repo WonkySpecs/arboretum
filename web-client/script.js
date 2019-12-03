c69406fd9cbc4c090a955b019e3891e9635eb420
Will Taylor <w1ll100@hotmail.co.uk>
2019-12-03 21:06:43 +0000

    Tiny number + click examples

--- web-client/script.js
+++ web-client/script.js
@@ -25,9 +25,17 @@ function texturesLoaded(app) {
     let textures = splitSpriteSheet(PIXI.loader.resources["spritesheet.png"].texture, app);
     for (let i = 0; i < 1000; i++ ) {
         let spriteIndex = Math.floor(Math.random() * config.cardSprites.length)
-        let card = PIXI.Sprite.from(textures[config.cardSprites[spriteIndex]]);
+        let suit = config.cardSprites[spriteIndex];
+        let card = PIXI.Sprite.from(textures[suit]);
         card.x = Math.random() * (config.canvas.width - card.width);
         card.y = Math.random() * (config.canvas.height - card.height);
+        let num = Math.floor(Math.random() * 8) + 1;
+        let text = new PIXI.Text(num.toString())
+        text.x = 12;
+        text.y = 15;
+        card.addChild(text);
+        card.interactive = true;
+        card.on('pointerdown', _ => console.log("Clicked " + num.toString() + " of " + suit));
         app.stage.addChild(card);
     }
     document.body.appendChild(app.view);
