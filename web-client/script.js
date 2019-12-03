8011778ea2526fa02d24402755d3874b2dd10a3c
Will Taylor <w1ll100@hotmail.co.uk>
2019-12-03 20:51:05 +0000

    Load card spritesheet

--- web-client/script.js
+++ web-client/script.js
@@ -1,14 +1,45 @@
+config = {
+    canvas: { width: 600, height: 400 },
+    cardSprites: [
+        "maple",
+        "cassia",
+        "cherryBlossom",
+        "spruce",
+        "jacaranda",
+        "oak",
+        "dogwood",
+        "royalPoinciana",
+        "willow",
+        "tulipPoplar"],
+    cardSpriteWidth: 40,
+}
 window.onload = function() {
-    let app = new PIXI.Application({width: 600, height: 400, antialias: true});
-    document.body.appendChild(app.view);
+    let app = new PIXI.Application({width: config.canvas.width, height: config.canvas.height, antialias: true});
     PIXI.loader
-        .add("testImage.png")
-        .load(() => texturesLoaded(app, ws))
+        .add("spritesheet.png")
+        .load(() => texturesLoaded(app))
+    console.log(app);
 }
 
-function texturesLoaded(app, ws) {
-    let sprite = new PIXI.Sprite(
-        PIXI.loader.resources["testImage.png"].texture);
-    app.stage.addChild(sprite);
+function texturesLoaded(app) {
+    let textures = splitSpriteSheet(PIXI.loader.resources["spritesheet.png"].texture, app);
+    for (let i = 0; i < 1000; i++ ) {
+        let spriteIndex = Math.floor(Math.random() * config.cardSprites.length)
+        let card = PIXI.Sprite.from(textures[config.cardSprites[spriteIndex]]);
+        card.x = Math.random() * (config.canvas.width - card.width);
+        card.y = Math.random() * (config.canvas.height - card.height);
+        app.stage.addChild(card);
+    }
+    document.body.appendChild(app.view);
 }
 
+function splitSpriteSheet(sheet, app) {
+    let cardHeight = sheet.height;
+    let textures = {};
+    for ( let i = 0; i < config.cardSprites.length; i++ ) {
+        textures[config.cardSprites[i]] = new PIXI.Texture(
+            sheet,
+            new PIXI.Rectangle(i * config.cardSpriteWidth, 0, config.cardSpriteWidth, cardHeight));
+    }
+    return textures;
+}
