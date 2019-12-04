05429573e20df4d8e559bd7c42498d3c9827297c
Will Taylor <w1ll100@hotmail.co.uk>
2019-12-04 19:38:04 +0000

    Card builder

--- web-client/script.js
+++ web-client/script.js
@@ -1,6 +1,6 @@
 config = {
-    canvas: { width: 600, height: 400 },
-    cardSprites: [
+    canvas: { width: 1080, height: 720 },
+    suits: [
         "maple",
         "cassia",
         "cherryBlossom",
@@ -12,42 +12,59 @@ config = {
         "willow",
         "tulipPoplar"],
     cardSpriteWidth: 40,
+    wsURL: "ws://127.0.0.1"
 }
+
 window.onload = function() {
     let app = new PIXI.Application({width: config.canvas.width, height: config.canvas.height, antialias: true});
     PIXI.loader
         .add("spritesheet.png")
         .load(() => texturesLoaded(app))
-    console.log(app);
 }
 
 function texturesLoaded(app) {
     let textures = splitSpriteSheet(PIXI.loader.resources["spritesheet.png"].texture, app);
+    let builder = cardBuilder(textures);
     for (let i = 0; i < 1000; i++ ) {
-        let spriteIndex = Math.floor(Math.random() * config.cardSprites.length)
-        let suit = config.cardSprites[spriteIndex];
-        let card = PIXI.Sprite.from(textures[suit]);
-        card.x = Math.random() * (config.canvas.width - card.width);
-        card.y = Math.random() * (config.canvas.height - card.height);
-        let num = Math.floor(Math.random() * 8) + 1;
-        let text = new PIXI.Text(num.toString())
-        text.x = 12;
-        text.y = 15;
-        card.addChild(text);
-        card.interactive = true;
-        card.on('pointerdown', _ => console.log("Clicked " + num.toString() + " of " + suit));
-        app.stage.addChild(card);
+        let card = builder.build(Math.floor(Math.random() * 8) + 1, config.suits[Math.floor(Math.random() * config.suits.length)]);
+        card.sprite.x = Math.random() * (config.canvas.width - card.sprite.width);
+        card.sprite.y = Math.random() * (config.canvas.height - card.sprite.height);
+        app.stage.addChild(card.sprite);
     }
+    console.log(app.stage);
     document.body.appendChild(app.view);
 }
 
 function splitSpriteSheet(sheet, app) {
     let cardHeight = sheet.height;
     let textures = {};
-    for ( let i = 0; i < config.cardSprites.length; i++ ) {
-        textures[config.cardSprites[i]] = new PIXI.Texture(
+    for ( let i = 0; i < config.suits.length; i++ ) {
+        textures[config.suits[i]] = new PIXI.Texture(
             sheet,
             new PIXI.Rectangle(i * config.cardSpriteWidth, 0, config.cardSpriteWidth, cardHeight));
     }
     return textures;
+}
+
+function cardBuilder(textures, inputManager) {
+    return {
+        build: function(value, suit) {
+            let card = { val: value, suit: suit };
+            let sprite = PIXI.Sprite.from(textures[suit]);
+            sprite.on('pointerdown', _ => cardClicked(card));
+            sprite.interactive = true;
+
+            let text = new PIXI.Text(value.toString())
+            text.x = 12;
+            text.y = 15;
+            sprite.addChild(text);
+            card.sprite = sprite;
+
+            return card;
+        }
+    };
+}
+
+function cardClicked(card) {
+    console.log("Clicked " + card.val + " of " + card.suit);
 }
